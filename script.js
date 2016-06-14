f65c2d2ae237d2982c3a872117017c98e43c45d3
hannahemilylane <hannahemilylane@gmail.com>
2016-06-14 15:59:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+$(function() {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    $(window).scroll(function() {
+		
+		var distanceScrolled = Math.max(0, $(window).scrollTop());
+		var $navbar = $('nav');
+		var $navbarWrapper = $('.navbar-wrapper')
 
-      // Store hash
-      var hash = this.hash;
+		var pinPoint = $navbarWrapper.offset().top;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+		if (distanceScrolled >= pinPoint) {
+			$navbar.addClass('pinned');
+		} else {
+			$navbar.removeClass('pinned')
+		}
+	});
+    
+	$('a[href*="#"]').click(function(e) {
+		e.preventDefault();
+		var $target = $($(this).attr('href'));
+		var scrollTop = $target.offset().top;
+		$('html, body').animate({'scrollTop': scrollTop}, 500);
+	});
+
 });
